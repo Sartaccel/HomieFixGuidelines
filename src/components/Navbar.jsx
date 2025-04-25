e954772dffc4a09c61f26f3f78a591b4a0f66662
SHAJAI SJ <sjshajai56@gmail.com>
2025-04-25 16:49:26 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-
+import logo from '../assets/HomeFix.png';
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light px-3 border-bottom">
             <a className="navbar-brand d-flex align-items-center" href="#">
                 <img
-                    src="./src/assets/HomeFix.png"
+                    src={logo}
                     alt="Logo"
                     width="40"
                     height="40"
                     className="d-inline-block align-text-top me-2"
                 />
+
                 <span className="fw-bold fs-4">HomieFix</span>
             </a>
         </nav>
