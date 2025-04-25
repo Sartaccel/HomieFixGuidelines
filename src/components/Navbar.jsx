1d027af0942150623461cdbc2d7d232ed7df99dd
SHAJAI SJ <sjshajai56@gmail.com>
2025-04-25 16:46:00 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
             <a className="navbar-brand d-flex align-items-center" href="#">
                 <img
                     src="./src/assets/HomeFix.png"
-                    alt="HomieFix Logo"
+                    alt="Logo"
                     width="40"
                     height="40"
                     className="d-inline-block align-text-top me-2"
